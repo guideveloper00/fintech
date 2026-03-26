9c301f707f5f3c4aa775028ae7fac5f906eef173
guilherme <guilherm3goncalves@hotmail.com>
2026-03-25 23:22:14 -0300

    fix: refactor no path de source backend

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -1,12 +1,15 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { User } from '../users/entities/user.entity';
+import { Category } from '../categories/entities/category.entity';
+import { Transaction } from '../transactions/entities/transaction.entity';
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/database/migrations/*.ts'],
+  entities: [User, Category, Transaction],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
